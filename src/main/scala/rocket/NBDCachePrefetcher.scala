9eea389e73e768758becf5e640a7b3a6c3a68285
hwang <haoyuanwang102@gmail.com>
2020-03-14 17:08:57 -0700

    New stride prefetcher that support multiple streams

--- src/main/scala/rocket/NBDCachePrefetcher.scala
+++ src/main/scala/rocket/NBDCachePrefetcher.scala
@@ -76,8 +76,9 @@ class NLPrefetcherImpl(outer: NLPrefetcher) extends NBDCachePrefetcherImpl(outer
   val nl_req_addr = Reg(UInt(width = coreMaxAddrBits))
 
   var addr_valid = Bool()
-  addr_valid = io.cpu_req_addr > 0x010
-  // addr_valid = io.cpu_req_addr > 0x010 && io.cpu_mem_pc > UInt(0x80000000L, width = 64)
+  // addr_valid = io.cpu_req_addr > 0x010
+  // addr_valid = io.cpu_req_addr > UInt(0x80000000L, width = 64)
+  addr_valid = io.cpu_mem_pc > UInt(0x80000000L, width = 64) && io.cpu_req_addr > UInt(0x80000000L, width = 64)
 
   when (io.cpu_req_valid && addr_valid) {
     nl_req_valid := true.B
@@ -112,45 +113,190 @@ class NLPrefetcherImpl(outer: NLPrefetcher) extends NBDCachePrefetcherImpl(outer
   */
 
 
+class RPTEntry(id: Int)(p: Parameters) extends L1HellaCacheModule()(p) {
+  val dropCycles: Int = 1000
+
+  val io = new Bundle {
+    val cpu_req_valid  = Input(Bool())
+    val cpu_req_addr   = Input(UInt(width = coreMaxAddrBits))
+
+    val cpu_mem_pc     = Input(UInt(width = 64))
+
+
+    val cpu_req_pc_match = Output(Bool())
+    val prefetch_valid  = Output(Bool())
+    val prefetch_addr   = Output(UInt(width = coreMaxAddrBits))
+
+
+    val alloc_ready = Output(Bool())
+    val alloc_do = Input(Bool())
+
+    val state_steady   = Output(Bool())
+    val state_idle   = Output(Bool())
+    val state_trans  = Output(Bool())
+  }
+
+
+  val s_idle :: s_trans :: s_steady :: Nil = Enum(UInt(), 3)
+  val state = Reg(init=s_idle)
+
+
+  val rpte_pc = Reg(UInt(width = 64))
+  val rpte_addr = Reg(UInt(width = coreMaxAddrBits))
+  val rpte_stride = Reg(UInt())
+
+  val rpte_counter = new Counter(dropCycles)
+  val rpte_counter_wrap = RegInit(Bool(false))
+  rpte_counter_wrap := rpte_counter.inc()
+
+  val rpte_predict = Reg(UInt(width = coreMaxAddrBits))
+
+  val est_stride = Reg(UInt())
+
+  val cpu_req_pc_match = Wire(Bool())
+
+
+  io.alloc_ready := state === s_idle
+  io.state_idle := state === s_idle
+  io.state_trans := state === s_trans
+  io.state_steady := state === s_steady
+
+  cpu_req_pc_match := io.cpu_req_valid && io.cpu_mem_pc === rpte_pc
+  io.cpu_req_pc_match := cpu_req_pc_match && state =/= s_idle
+
+
+  when (cpu_req_pc_match) {
+    rpte_counter.value := 0
+  }
+
+
+  when (state === s_idle) {
+    when (io.alloc_do && io.cpu_req_valid) {
+      state := s_trans
+      rpte_pc := io.cpu_mem_pc
+      rpte_addr := io.cpu_req_addr
+      rpte_stride := 0
+    }
+  }
+
+  when (state === s_trans) {
+    when (cpu_req_pc_match) {
+      est_stride := io.cpu_req_addr - rpte_addr
+      rpte_addr := io.cpu_req_addr
+      when (est_stride === rpte_stride) {
+        // correct, go to steady
+        state := s_steady
+      }
+      rpte_stride := est_stride
+    }
+  }
+
+  when (state === s_steady) {
+    when (cpu_req_pc_match) {
+      rpte_predict := io.cpu_req_addr + rpte_stride
+
+      est_stride := io.cpu_req_addr - rpte_addr
+      rpte_addr := io.cpu_req_addr
+
+      when (est_stride =/= rpte_stride) {
+        // incorrect, go to transient
+        state := s_trans
+        rpte_stride := est_stride
+      }
+    }
+  }
+
+
+  when (RegNext(cpu_req_pc_match && state === s_steady)) {
+    io.prefetch_valid := true.B
+    io.prefetch_addr := rpte_predict
+  } .otherwise {
+    io.prefetch_valid := false.B
+  }
+
+
+  when (rpte_counter_wrap) {
+    state := s_idle
+  }
+
+
+}
+
+
+
+class RPTFile(tableSize: Int)(p: Parameters) extends L1HellaCacheModule()(p) {
+  val io = new Bundle {
+    val cpu_req_valid  = Input(Bool())
+    val cpu_req_addr   = Input(UInt(width = coreMaxAddrBits))
+
+    val cpu_mem_pc     = Input(UInt(width = 64))
+
+    val prefetch_req_addr   = Output(UInt(width = coreMaxAddrBits))
+    val prefetch_req_valid = Output(Bool())
+  }
+
+  val req_valid = RegInit(false.B)
+  val req_addr  = Reg(UInt(width = coreMaxAddrBits))
+
+  var pc_match = Bool(false)
+
+  val pref_arb = Module(new Arbiter(UInt(width = coreMaxAddrBits), tableSize))
+  val alloc_arb = Module(new Arbiter(Bool(), tableSize))
+
+  val RPTEs = (0 until tableSize) map { i =>
+    val rpte = Module(new RPTEntry(i)(p))
+
+    rpte.io.cpu_req_valid := io.cpu_req_valid
+    rpte.io.cpu_req_addr := io.cpu_req_addr
+    rpte.io.cpu_mem_pc := io.cpu_mem_pc
+
+    pref_arb.io.in(i).valid := rpte.io.prefetch_valid
+    pref_arb.io.in(i).bits := rpte.io.prefetch_addr
+
+    alloc_arb.io.in(i).valid := rpte.io.alloc_ready
+    rpte.io.alloc_do := alloc_arb.io.in(i).ready
+
+    pc_match = pc_match || rpte.io.cpu_req_pc_match
+
+    rpte
+  }
+
+  // io.req.valid && sdq_rdy && cacheable && !idx_match
+  var addr_valid = Bool()
+  // addr_valid = io.cpu_req_addr > 0x010
+  addr_valid = io.cpu_mem_pc > UInt(0x80000000L, width = 64) && io.cpu_req_addr > UInt(0x80000000L, width = 64)
+  // addr_valid = io.cpu_req_addr > UInt(0x80000000L, width = 64)
+
+  alloc_arb.io.out.ready := io.cpu_req_valid && !pc_match && addr_valid
+
+  io.prefetch_req_valid := pref_arb.io.out.valid
+  io.prefetch_req_addr := pref_arb.io.out.bits
+}
+
 class StridePrefetcher(implicit p: Parameters) extends NBDCachePrefetcher()(p) {
   override lazy val module = new StridePrefetcherImpl(this)
 }
 
 class StridePrefetcherImpl(outer: StridePrefetcher) extends NBDCachePrefetcherImpl(outer) {
-  // val degree = 3
 
-  val req_addr_history = Reg(UInt(width = coreMaxAddrBits))
-  val req_stride = Reg(UInt(width = coreMaxAddrBits))
-
-
-  when (io.cpu_req_valid) {
-    req_stride := io.cpu_req_addr - req_addr_history
-    req_addr_history := io.cpu_req_addr
-  }
-
-
-  val stride_req_valid = RegInit(false.B)
-  val stride_req_addr = Reg(UInt(width = coreMaxAddrBits))
-
-  when (io.cpu_req_valid) {
-    when (req_stride < UInt(0x1000)) {
-      stride_req_valid := true.B
-      stride_req_addr  := io.cpu_req_addr + req_stride
-    }.otherwise {
-      stride_req_valid := false.B
-    }
-  } .otherwise {
-    stride_req_valid := false.B
-  }
-
-  // Handshake
   val req_valid = RegInit(false.B)
-  val req_addr = RegEnable(stride_req_addr, stride_req_valid)
 
-  when (io.prefetch_req.fire() && !stride_req_valid) {
+  val rpts = Module(new RPTFile(8)(outer.p))
+
+  rpts.io.cpu_req_valid := RegNext(io.cpu_req_valid)
+  rpts.io.cpu_req_addr := RegNext(io.cpu_req_addr)
+
+  rpts.io.cpu_mem_pc := io.cpu_mem_pc
+
+
+
+  val req_addr = RegEnable(rpts.io.prefetch_req_addr, rpts.io.prefetch_req_valid)
+
+
+  when (io.prefetch_req.fire() && !rpts.io.prefetch_req_valid) {
     req_valid := false.B
   } .otherwise {
-    when (stride_req_valid) {
+    when (rpts.io.prefetch_req_valid) {
       req_valid := true.B
     }
   }
@@ -160,5 +306,6 @@ class StridePrefetcherImpl(outer: StridePrefetcher) extends NBDCachePrefetcherIm
   io.prefetch_req.bits.addr        := req_addr
   io.prefetch_req.bits.cmd         := M_PFW
   io.prefetch_req.bits.data        := UInt(0)
+
 
 }
